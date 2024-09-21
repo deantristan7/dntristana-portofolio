1ccc94279342c06c0ee45f3aa9f468bee4711fc1
Dean Tristan <tristandean7@gmail.com>
2024-09-22 00:21:45 +0700

    chore: Add env file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+
   return (
     <Html lang="en">
       <title>Dean Tristan | Full-Stack Developer</title>
@@ -14,17 +16,17 @@ export default function Document() {
         {/* Open Graph / Facebook */}
         <meta property="og:title" content="Dean Tristan | Full-Stack Developer" />
         <meta property="og:description" content="Discover the projects, skills, and experience of Dean Tristan, a full-stack developer with a passion for coding and dark arts." />
-        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:image" content={`${vercelUrl}/images/pentagram.png`} />
+        <meta property="og:url" content={vercelUrl} />
         <meta property="og:type" content="website" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Dean Tristan | Full-Stack Developer" />
         <meta name="twitter:description" content="Explore the personal portfolio of Dean Tristan, a full-stack developer specializing in Go, PHP, and metal-themed projects." />
-        <meta name="twitter:image" content="/images/og-image.png" />
-        
-        <link rel="icon" type="image/x-icon" href="/images/pentagram.png" />
+        <meta name="twitter:image" content={`${vercelUrl}/images/pentagram.png`} />
 
+        <link rel="icon" type="image/x-icon" href="/images/pentagram.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap"
           rel="stylesheet"
